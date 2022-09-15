75c65340cdefac40b455026e9afd841af7bf16b9
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-09-15 09:04:31 -0600

    feat: test one finished

--- spec/models/aroc_week_1_spec.rb
+++ spec/models/aroc_week_1_spec.rb
@@ -9,16 +9,15 @@ describe 'ActiveRecord Obstacle Course, Week 1' do
 # Here are the docs associated with ActiveRecord queries: http://guides.rubyonrails.org/active_record_querying.html
 
 # ----------------------
-
-
   it '1. finds orders by amount' do
     # ----------------------- Using Ruby -------------------------
-    orders_of_500 = Order.all.select { |order| order.amount == 500 }
-    orders_of_200 = Order.all.select { |order| order.amount == 200 }
+    # orders_of_500 = Order.all.select { |order| order.amount == 500 }
+    # orders_of_200 = Order.all.select { |order| order.amount == 200 }
     # ------------------------------------------------------------
 
     # ------------------ Using ActiveRecord ----------------------
-    # Solution goes here
+    orders_of_500 = Order.where("amount = 500")
+    orders_of_200 = Order.where("amount = 200")
     # ------------------------------------------------------------
 
     # Expectation
